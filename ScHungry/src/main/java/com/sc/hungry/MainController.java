e49833ebcece93f96e506bddba0ec554b190bd6c
dae12 <dae12@localhost>
2023-09-08 08:54:16 +0900

    테스트 완

--- ScHungry/src/main/java/com/sc/hungry/MainController.java
+++ ScHungry/src/main/java/com/sc/hungry/MainController.java
@@ -9,9 +9,9 @@ public class MainController {
 	public String index() {
 		return "/index";
 	}
-	@GetMapping("/hello")
-	public String hello() {
-		return "/hello";
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
 	}
 	
 	@GetMapping("/welcome")
