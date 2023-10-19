fd8bba26a628df97a5862a4e89b98d3a543f9ba3
dae12 <dae12@218.38.137.28>
2023-10-19 23:33:31 +0900

    test

--- ScHungry/src/main/java/com/sc/hungry/controller/loginController.java
+++ ScHungry/src/main/java/com/sc/hungry/controller/loginController.java
@@ -1,5 +1,9 @@
 package com.sc.hungry.controller;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -43,5 +47,30 @@ public class loginController {
 	public String main() {
 		System.out.println("main controller:");
         return "main/main";
+    }
+	
+	@GetMapping(value="/getMac")
+	public String getMac() {
+		String result = "";
+		InetAddress ip;
+		logger.debug("juso getMac::");
+		try {
+			ip = InetAddress.getLocalHost();
+		   
+			NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+			byte[] mac = network.getHardwareAddress();
+		   
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+			}
+				result = sb.toString();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e){
+			e.printStackTrace();
+		}
+		    
+		return result;
     }
 }
--- ScHungry/src/main/java/com/sc/hungry/controller/mainController.java
+++ ScHungry/src/main/java/com/sc/hungry/controller/mainController.java
@@ -1,12 +1,46 @@
 package com.sc.hungry.controller;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class mainController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("main")	
-	public String home() {
+	public String main() {
 		return "main/main";
 	}
 	
-	
+	@RequestMapping(value="/getMac", method=RequestMethod.POST)
+	public String getMac() {
+		String result = "";
+		InetAddress ip;
+		logger.debug("juso getMac::");
+		try {
+			ip = InetAddress.getLocalHost();
+		   
+			NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+			byte[] mac = network.getHardwareAddress();
+		   
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+			}
+				result = sb.toString();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e){
+			e.printStackTrace();
+		}
+		    
+		return result;
+    }
 }
